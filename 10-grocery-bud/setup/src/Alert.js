53cb674fad699997bc13da366ba0c27b28b0efc8
Danilo Ninkovic <ninkovicdanilo@hotmail.com>
2022-12-11 17:44:37 +0100

    finished grocery bud

--- 10-grocery-bud/setup/src/Alert.js
+++ 10-grocery-bud/setup/src/Alert.js
@@ -1,7 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
-const Alert = () => {
-  return <h2>alert component</h2>
-}
+/* remove alert smo postavili u samoj Alert componenti
+jer ce njeno renderovanje trigerovati useEffect 
+pomocu kojeg cemo, kad se comp. renderuje ,[] 
+postaviti remove alert poslije 2 sec
 
-export default Alert
+removeAlert je u stvari showAlert ali ce poslije remove 
+biti show:false
+*/
+const Alert = ({ msg, type, removeAlert, list }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 2000);
+    return () => clearTimeout(timeout); //clean up
+  }, [list]); //kad god se promjeni list[]
+  return <p className={`alert alert-${type}`}>{msg}</p>;
+};
+
+export default Alert;
