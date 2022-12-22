b3830d185c330bf80654aa9bd33f24ed31687090
danilo-ninkovic <ninkovicdanilo@hotmail.com>
2022-12-22 19:49:36 +0100

    setCoctails

--- 15-cocktails/setup/src/components/CocktailList.js
+++ 15-cocktails/setup/src/components/CocktailList.js
@@ -1,14 +1,26 @@
-import React from 'react'
-import Cocktail from './Cocktail'
-import Loading from './Loading'
-import { useGlobalContext } from '../context'
+import React from "react";
+import Cocktail from "./Cocktail";
+import Loading from "./Loading";
+import { useGlobalContext } from "../context";
 
 const CocktailList = () => {
+  const { coctails, loading } = useGlobalContext();
+  console.log(coctails);
+  if (loading) {
+    return <Loading />;
+  }
+  if (coctails.length < 1) {
+    return (
+      <h2 className="section-title">
+        no coctails matched your search criteria
+      </h2>
+    );
+  }
   return (
     <div>
       <h2>cocktail list component</h2>
     </div>
-  )
-}
+  );
+};
 
-export default CocktailList
+export default CocktailList;
--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero aliquam,
+        eius voluptas eos quidem quo exercitationem? Illum harum doloribus
+        exercitationem dolores dolorem consequatur beatae, recusandae fugit
+        iusto ipsum distinctio pariatur, officiis eius cum, sit aut non.
+        Deleniti saepe dolor libero.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
